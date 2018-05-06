ce52582fb8bcbe3343d67f486813fa5d4dd3e2b3
Georg Grob <grobgl@gmail.com>
2018-05-06 22:15:55 +0100

    Fix speedtest

--- speedtest/run.py
+++ speedtest/run.py
@@ -21,7 +21,7 @@ def run_threaded(job_func):
 global db
 db = InfluxDBClient(host='influxdb', port=8086, username='root', password='root', database='speedtest')
 
-db.create_retention_policy('forever', 'INF', 1)
+# db.create_retention_policy('forever', 'INF', 1)
 
 schedule.every(5).minutes.do(run_threaded, job)
 
