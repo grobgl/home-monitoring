819138e0d36a83a1ce0f3bc7adaa34868ebbf57d
Georg Grob <grobgl@gmail.com>
2018-05-06 21:33:29 +0100

    Reset speedtest runner

--- speedtest/run.py
+++ speedtest/run.py
@@ -3,13 +3,9 @@ import speedtest
 import time
 
 def job():
-    print("Start speedtest")
     s = speedtest.Speedtest()
-    print("get best server")
     s.get_best_server()
-    print("download")
     s.download()
-    print("upload")
     s.upload()
     print(s.results.dict())
 
@@ -17,10 +13,9 @@ def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
 
-job()
-# schedule.every(5).minutes.do(run_threaded, job)
+schedule.every(5).minutes.do(run_threaded, job)
 
-# while 1:
-#     schedule.run_pending()
-#     time.sleep(1)
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
 
