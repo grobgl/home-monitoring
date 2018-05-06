292748a600a35970e9db14e51626b3be4789ebca
Georg Grob <grobgl@gmail.com>
2018-05-06 21:55:13 +0100

    Connect speedtest to db

--- speedtest/run.py
+++ speedtest/run.py
@@ -2,6 +2,7 @@ import schedule
 import speedtest
 import time
 
+
 def job():
     s = speedtest.Speedtest()
     s.get_best_server()
@@ -12,6 +13,16 @@ def job():
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
+
+
+db = InfluxDBClient(
+    host='influxdb',
+    port=8086,
+    username='root',
+    password='root',
+    database='speedtest')
+
+db.create_retention_policy('forever', 'INF')
 
 schedule.every(5).minutes.do(run_threaded, job)
 
