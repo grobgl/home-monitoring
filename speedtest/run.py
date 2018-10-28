3ea4333d4e8d6e2c2f63c7405fb3b47044e6f1f4
Georg Grob <grobgl@gmail.com>
2018-10-28 17:30:04 +0000

    Logging

--- speedtest/run.py
+++ speedtest/run.py
@@ -1,3 +1,4 @@
+import logging
 import schedule
 import speedtest
 import time
@@ -5,20 +6,27 @@ from threading import Thread
 from influxdb import InfluxDBClient
 
 
+_LOGGER = logging.getLogger()
+
+
 def job():
+    _LOGGER.info('Running speedtest ...')
     s = speedtest.Speedtest()
     s.get_best_server()
     s.download()
     s.upload()
     res = s.results.dict()
-    res = [ 
-              { 
+
+    _LOGGER.info('Speedtest results: download %d, upload %d, ping %d', res['download'], res['upload'], res['ping'])
+
+    res = [
+              {
                   'measurement': key,
                   'fields': {
-                      'Int_value': res[key] 
+                      'Int_value': res[key]
                   },
                   'tags': []
-              } for key in ['download', 'upload', 'ping'] 
+              } for key in ['download', 'upload', 'ping']
           ]
     db.write_points(res)
 
@@ -27,6 +35,8 @@ def run_threaded(job_func):
     job_thread.start()
 
 
+_LOGGER.info('Initialising ...')
+
 global db
 db = InfluxDBClient(host='influxdb', port=8086, username='root', password='root', database='speedtest')
 db.create_database('speedtest')
@@ -34,6 +44,8 @@ db.create_retention_policy('forever', 'INF', 1, default=True)
 db.switch_user("dbuser", "dbuser")
 
 schedule.every(5).minutes.do(run_threaded, job)
+
+_LOGGER.info('Initialised')
 
 while 1:
     schedule.run_pending()
